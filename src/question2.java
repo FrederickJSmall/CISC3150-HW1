ce035ade329a96d2772bd74d99c77b2b4fde9ebd
Fred Small <Fsmall@startoffsmall.com>
2015-09-05 18:53:23 -0400

    add logic for calculating the radius

--- src/question2.java
+++ src/question2.java
@@ -1,10 +1,29 @@
-
+import java.util.Scanner;
 
 public class question2 {
 	public static void main (String[] args)
 	{
-		System.out.println("question 2");
-		
+		// Math.PI
+		Scanner keyboard = new Scanner(System.in);
+		while(true)
+		{
+			System.out.println("Enter a radius");
+			int radius = keyboard.next().charAt(0);
+			char keyChar = (char) radius;
+			if (keyChar==4)
+			{
+				keyboard.close();
+				break;
+			}
+			if ((keyChar >=48) && (keyChar <=57))
+			{
+				System.out.println("Radius=" + radius);
+				System.out.println("Area  =" + Math.PI * (Math.pow(radius,2)));
+				System.out.println("------------------------------");				
+			}
+			else
+				System.out.println("Please type a proper number");
+		}
 	}
 }
 
