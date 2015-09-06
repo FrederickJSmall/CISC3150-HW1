35b0a38ca64ea5d1bf13593e71cf6735cf2e3fcf
Fred Small <Fsmall@startoffsmall.com>
2015-09-05 21:18:09 -0400

    code to print a random month

--- src/question3.java
+++ src/question3.java
@@ -1,9 +1,43 @@
-
+import java.util.Random;
 
 public class question3 {
 	public static void main (String[] args)
 	{
-		System.out.println("question 3");
-	}
+		Random randomMonth = new Random();
+		int month = randomMonth.nextInt(12) + 1;
+
+        String monthInYear;
+        switch (month) {
+            case 1:  monthInYear = "January";
+                     break;
+            case 2:  monthInYear = "February";
+                     break;
+            case 3:  monthInYear = "March";
+                     break;
+            case 4:  monthInYear = "April";
+                     break;
+            case 5:  monthInYear = "May";
+                     break;
+            case 6:  monthInYear = "June";
+                     break;
+            case 7:  monthInYear = "July";
+                     break;
+            case 8:  monthInYear = "August";
+                     break;
+            case 9:  monthInYear = "September";
+                     break;
+            case 10: monthInYear = "October";
+                     break;
+            case 11: monthInYear = "November";
+                     break;
+            case 12: monthInYear = "December";
+                     break;
+            default: monthInYear = "Invalid month";
+                     break;
+        }
+        System.out.println(monthInYear);
+    }
+
 }
+
 
